5a69a8ca09cb58ce5e575361dc3c00cc0739480a
NatashaAntoshina <antosinanatala27@gmail.com>
2024-02-15 07:42:22 +0300

    15.02.2024

--- main.py
+++ main.py
@@ -1,68 +1,108 @@
 import pygame
+import os
 from random import *
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 800, 900
+MOVING_GUN = 15
+MOVING_LASER = 8
+FPS = 15
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('image', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    #else:
+    #   image = image.convert_alpha()
+    return image
 
 
 class Gun():
-    def __init__(self, x, y, corner):
-        self.x = x
-        self.y = y
-        self.corn = corner
+    def __init__(self):
+        self.width = 30
+        self.height = 50
+        self.x = WINDOW_WIDTH // 2 - self.width // 2
+        self.y = 780
 
-    def calculation(self):
-        x2 = 395
-        y2 = 780
-        x3 = 405
-        y3 = 780
-        h = 70
-        self.point1 = 1
+    def move(self):
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            if self.x > (WINDOW_WIDTH // 2) % MOVING_GUN:
+                self.x = self.x - MOVING_GUN
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            if self.x < WINDOW_WIDTH - (WINDOW_WIDTH // 2) % MOVING_GUN - self.width:
+                self.x = self.x + MOVING_GUN
+
+    def gun_size(self):
+        return self.width, self.height
+
+    def get_position(self):
+        return self.x, self.y
 
     def drawing(self, screen):
-        pygame.draw.polygon(screen, pygame.Color('Orange'), [(380, 850), (395, 780), (405, 780), (420, 850)], 0)
+        pygame.draw.rect(screen, pygame.Color('Orange'), (self.x, self.y, self.width, self.height), 0)
+
 
 class Laser():
     def __init__(self):
-        self.mm = 0
+        self.width = 6
+        self.height = 30
+        self.x = 350
+        self.y = 350
+
+    def render(self, screen):
+        pygame.draw.rect(screen, pygame.Color('Green'), (self.x, self.y, self.width, self.height), 0)
+
+    def fire(self, screen, position, size):
+        self.x = position[0] + size[0] // 2 - self.width // 2
+        self.y = position[1] - self.height
+        pygame.draw.rect(screen, pygame.Color('Green'), (self.x, self.y, self.width, self.height), 0)
+
+    def move(self):
+        self.y = self.y - MOVING_LASER
+
+    def get_size(self):
+        return self.width, self.height
+
+    def get_position(self):
+        return self.x, self.y
 
 
-# if __name__ == '__main__':
-#     pygame.init()
-#     size = width, height = 800, 900
-#     screen = pygame.display.set_mode(size)
-#
-#     # формирование кадра:
-#     # команды рисования на холсте
-#     # ...
-#     # ...
-#     # смена (отрисовка) кадра:
-#     pygame.display.flip()
-#     # ожидание закрытия окна:
-#     while pygame.event.wait().type != pygame.QUIT:
-#         pass
-#     # завершение работы:
-#     pygame.quit()
-
-
-if __name__ == '__main__':
+def main():
+    image = load_image("космический пейзаж.jpg")
     pygame.init()
     pygame.display.set_caption('Space Defender')
-    size = width, height = 800, 900
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    flag = False
 
-    for i in range(10000):
-        screen.fill(pygame.Color('white'),
-                    (random() * width,
-                     random() * height, 1, 1))
-
-    running = True
+    gun = Gun()
+    laser = Laser()
     clock = pygame.time.Clock()
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        gun = Gun(3, 4, 6)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    laser.fire(screen, gun.get_position(), gun.gun_size())
+                    flag = True
+        gun.move()
+        screen.fill((0, 0, 0))
+        screen.blit(image, (0, 0))
         gun.drawing(screen)
-        #screen.fill((0, 0, 0))
-        #pygame.draw.circle(screen, (255, 0, 0), (int(x_pos), 200), 20)
-        #x_pos += v * clock.tick() / 1000  # v * t в секундах
+        if flag:
+            laser.move()
+            laser.render(screen)
+            if laser.get_position()[1] - laser.get_size()[1] >= WINDOW_HEIGHT:
+                flag = False
         pygame.display.flip()
+        clock.tick(FPS)
     pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
